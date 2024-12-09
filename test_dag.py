0e58b7d2a882a41b442d8d46caf00f3b39742e6d
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-12-09 23:11:10 +0800

    add test cases

--- test_dag.py
+++ test_dag.py
@@ -1,6 +1,7 @@
 import pytest
 from dag import DAG
 
+
 def test_add_vertex():
     dag = DAG()
     dag.add_vertex('A')
@@ -14,20 +15,22 @@ def test_add_edge():
     assert 'B' in dag.graph
     assert 'B' in dag.graph['A']
 
-def test_has_cycle():
+@pytest.mark.parametrize("method", ['iterative', 'recursive'])
+def test_has_cycle(method):
     dag = DAG()
     dag.add_edge('A', 'B')
     dag.add_edge('B', 'C')
-    assert not dag.has_cycle()
+    assert not dag.has_cycle(method)
     dag.add_edge('C', 'A')
-    assert dag.has_cycle()
+    assert dag.has_cycle(method)
 
-def test_topological_sort():
+@pytest.mark.parametrize("method", ['iterative', 'recursive'])
+def test_topological_sort(method):
     dag = DAG()
     dag.add_edge('A', 'B')
     dag.add_edge('B', 'C')
     dag.add_edge('A', 'C')
-    assert dag.topological_sort() in [['A', 'B', 'C'], ['A', 'C', 'B']]
+    assert dag.topological_sort(method) in [['A', 'B', 'C'], ['A', 'C', 'B']]
 
 if __name__ == "__main__":
     pytest.main()
